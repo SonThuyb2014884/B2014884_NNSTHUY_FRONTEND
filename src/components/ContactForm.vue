8ce4d9e53034478a1fef582128a150f5b74c6431
Your Name <you@example.com>
2023-10-31 00:19:04 +0700

    Tao trang them moi mot lien he

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,68 +1,36 @@
 <template>
-    <Form 
-    @submit="submitContact" 
-    :validation-schema="contactFormSchema"
-    >
+    <Form @submit="submitContact" 
+    :validation-schema="contactFormSchema">
         <div class="form-group">
             <label for="name">Tên</label>
-            <Field 
-            name="name" 
-            type="text" 
-            class="form-control" 
-            v-model="contactLocal.name" 
-            />
-            <ErrorMessage 
-            name="name" 
-            class="error-feedback" />
+            <Field name="name" type="text" class="form-control" v-model="contactLocal.name" />
+            <ErrorMessage name="name" class="error-feedback" />
         </div>
-        <div 
-        class="form-group">
+        <div class="form-group">
             <label for="email">E-mail</label>
-            <Field 
-            name="email" 
-            type="email" 
-            class="form-control" 
-            v-model="contactLocal.email" 
-            />
-            <ErrorMessage 
-            name="email" 
-            class="error-feedback" />
+            <Field name="email" type="email" class="form-control" v-model="contactLocal.email" />
+            <ErrorMessage name="email" class="error-feedback" />
         </div>
         <div class="form-group">
             <label for="address">Địa chỉ</label>
-            <Field name="address" 
-            type="text" 
-            class="form-control" 
-            v-model="contactLocal.address" />
-            <ErrorMessage name="address" 
-            class="error-feedback" />
+            <Field name="address" type="text" class="form-control" v-model="contactLocal.address" />
+            <ErrorMessage name="address" class="error-feedback" />
         </div>
         <div class="form-group">
             <label for="phone">Điện thoại</label>
-            <Field name="phone" 
-            type="tel" 
-            class="form-control" 
-            v-model="contactLocal.phone" />
-            <ErrorMessage name="phone" 
-            class="error-feedback" />
+            <Field name="phone" type="tel" class="form-control" v-model="contactLocal.phone" />
+            <ErrorMessage name="phone" class="error-feedback" />
         </div>
         <div class="form-group form-check">
-            <input name="favorite" 
-            type="checkbox" 
-            class="form-check-input" 
-            v-model="contactLocal.favorite" />
-            <label for="favorite" 
-            class="form-check-label">
+            <input name="favorite" type="checkbox" class="form-check-input" v-model="contactLocal.favorite" />
+            <label for="favorite" class="form-check-label">
                 <strong>Liên hệ yêu thích</strong>
             </label>
         </div>
         <div class="form-group">
             <button class="btn btn-primary"> <i class="fa-solid fa-floppy-disk "></i>Lưu</button>
-            <button v-if="contactLocal._id" 
-            type="button" 
-            class="ml-2 btn btn-danger" 
-            @click="deleteContact">
-            <i class="fa-solid fa-trash"></i>
+            <button v-if="contactLocal._id" type="button" class="ml-2 btn btn-danger" @click="deleteContact">
+                <i class="fa-solid fa-trash"></i>
                 Xóa
             </button>
         </div>
@@ -94,7 +62,9 @@ export default {
                 .string()
                 .email("E-mail không đúng.")
                 .max(50, "E-mail tối đa 50 ký tự."),
-            address: yup.string().max(100, "Địa chỉ tối đa 100 ký tự."),
+            address: yup
+            .string()
+            .max(100, "Địa chỉ tối đa 100 ký tự."),
             phone: yup
                 .string()
                 .matches(
